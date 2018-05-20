b3c2d60e8b95b2a0101de4dc814effc59e56291f
wangdesheng <737708900@qq.com>
2018-05-21 01:08:07 +0800

    Leaning Canvas

--- canvas/src/example2/index.js
+++ canvas/src/example2/index.js
@@ -7,24 +7,30 @@ window.onload = function () {
     ctx.lineWidth = 10
     ctx.fillStyle = 'red'
 
-    drawStar(ctx,50, 100, 110, 110)
+    drawStar(ctx, 50, 100, 110, 110, 30)
 };
 
-//
-function drawStar(ctx, r, R, x, y) {
+/**
+ * ctx canvas.getContext('2d')
+ * r  小圆半径
+ * R  大圆半径
+ * x,y 大小圆中心
+ * rote 旋转角度
+ * */
+function drawStar(ctx, r, R, x, y, rote=0) {
     ctx.beginPath();
     let arr = Array.from({length: 5});
     const pi = Math.PI / 180;
     arr.map((v, i) => {
         // 最外层5点
         ctx.lineTo(
-            Math.cos((18 + 72 * i) * pi) * R + x,
-            -Math.sin((18 + 72 * i) * pi) * R + y,
+            Math.cos((18 + 72 * i - rote) * pi) * R + x,
+            -Math.sin((18 + 72 * i - rote) * pi) * R + y,
         );
         // 里面5点
         ctx.lineTo(
-            Math.cos((54 + 72 * i) * pi) * r + x,
-            -Math.sin((54 + 72 * i) * pi) * r + y,
+            Math.cos((54 + 72 * i - rote) * pi) * r + x,
+            -Math.sin((54 + 72 * i - rote) * pi) * r + y,
         );
     });
     ctx.closePath();
